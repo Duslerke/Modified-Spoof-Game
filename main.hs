4cc715dfe296261a8797564753f79dfb6913fb55
Liudvikas Taluntis <liudvikast@gmail.com>
2020-03-20 21:21:56 +0000

    Added turn action function.
    Not sure if this is the best way to do it. Looking at this function screams of refactoring. But I don't know enough on how to work with Haskells IO, and what's a good standard. The thing is I need those logs!
    
    In short, this function takes a number of coins on the table and the player who's turn now has to be as its arguments.
    
    Based of player variable it decides on whether to call an AI code or the Player code and get his input. Both cases it returns a new number of coins as it's output (wrapped up in IO context).
    
    Guess I could refactor AI do block and Human do block into their own functions at some point.

--- main.hs
+++ main.hs
@@ -6,4 +6,39 @@ calculateNextMove currentCoins | distanceToControlSequence == 0 = 1
                                | otherwise = distanceToControlSequence
     where distanceToControlSequence = (currentCoins - 1) `mod` 4
 
--- Maybe - make monad f. Either a b -- if its losing, mod 4 will be 0
+
+data Player = PunyHuman | GloriousAI deriving (Eq, Show)
+
+
+turnAction :: Coins -> Player -> IO Coins
+turnAction coins player
+    | player == PunyHuman = do
+        putStrLn
+            $  "\nThere're currently  "
+            ++ show coins
+            ++ " coins on the table. Pick from 1-3: "
+        playerChoiceStr <- getLine
+        let playerChoice  = read playerChoiceStr :: Int
+        let leftOverCoins = coins - playerChoice
+        if leftOverCoins >= 0 && playerChoice > 0 && playerChoice < 4
+            then do
+                putStrLn
+                    $  "\nThe player took "
+                    ++ show playerChoice
+                    ++ " coins, "
+                    ++ show leftOverCoins
+                    ++ " remaining."
+                return leftOverCoins
+            else do
+                putStrLn "Can't do that! Learn to count!"
+                turnAction coins player
+    | player == GloriousAI = do
+        let aiChoice      = calculateNextMove coins
+        let leftOverCoins = coins - aiChoice
+        putStrLn
+            $  "AI has taken "
+            ++ show aiChoice
+            ++ " coins, "
+            ++ show leftOverCoins
+            ++ " remaining"
+        return leftOverCoins
